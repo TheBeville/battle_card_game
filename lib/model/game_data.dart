e569e9c047615422e2274d501939759c3a19b0c7
Steve Beville <steve_beville@hotmail.com>
2023-02-06 23:54:05 +0000

    created game_data model

---
+++ lib/model/game_data.dart
@@ -0,0 +1,21 @@
+class GameData {
+  final int playerScore;
+  final int opponentScore;
+  final int opponentCloseScore;
+  final int opponentRangedScore;
+  final int opponentSiegeScore;
+  final int playerCloseScore;
+  final int playerRangedScore;
+  final int playerSiegeScore;
+
+  const GameData({
+    required this.playerScore,
+    required this.opponentScore,
+    required this.opponentCloseScore,
+    required this.opponentRangedScore,
+    required this.opponentSiegeScore,
+    required this.playerCloseScore,
+    required this.playerRangedScore,
+    required this.playerSiegeScore,
+  });
+}
